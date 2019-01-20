c46cd4f7c2a232996333dd51a11549cd30586578
ChristianMarca <cmarcag@hotmail.com>
2019-01-20 14:56:17 -0500

    Updates

---
+++ precache-manifest.fca89592121a3996cce69c5c55e8831a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b14cde0fd7c9bc631030",
+    "url": "/sma_app/static/css/main.ea8b870a.chunk.css"
+  },
+  {
+    "revision": "b14cde0fd7c9bc631030",
+    "url": "/sma_app/static/js/main.b14cde0f.chunk.js"
+  },
+  {
+    "revision": "128b1b1fd6903790815e",
+    "url": "/sma_app/static/css/1.b68f5043.chunk.css"
+  },
+  {
+    "revision": "128b1b1fd6903790815e",
+    "url": "/sma_app/static/js/1.128b1b1f.chunk.js"
+  },
+  {
+    "revision": "362387bf06daa0055efe",
+    "url": "/sma_app/static/js/runtime~main.362387bf.js"
+  },
+  {
+    "revision": "9efeaae0f54c6a071d8da6946fb6b120",
+    "url": "/sma_app/index.html"
+  }
+];
